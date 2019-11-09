493b1786d03870769a1da07f4b982a5d8c9a27c6
Fernando Saraiva <fpsaraiva@gmail.com>
2019-11-09 15:00:09 -0300

    desafio aula 05 - final

--- aula_05/desafio_05.js
+++ aula_05/desafio_05.js
@@ -29,13 +29,17 @@ function anotherFunction(array, index) {
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-
+var myArray2 = ["Saraiva", true, 100, [1, 2, 3], {temp: 2}];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-
+console.log(anotherFunction(myArray2, 0));
+console.log(anotherFunction(myArray2, 1));
+console.log(anotherFunction(myArray2, 2));
+console.log(anotherFunction(myArray2, 3));
+console.log(anotherFunction(myArray2, 4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -51,30 +55,53 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-
+function book(bookName) {
+  var allBooks = {
+    'Skin in the game': {
+      quantidadePaginas: 236,
+      autor:'Taleb',
+      editora: 'Penguin'
+    },
+    'Introdução HTML': {
+      quantidadePaginas: 220,
+      autor: 'Lawson & Sharp',
+      editora: 'Alta'
+    },
+    'CSS avançado': {
+      quantidadePaginas: 283,
+      autor:'Meyer',
+      editora: 'Bookman'
+    }
+  };
+  
+  return !bookName ? allBooks : allBooks[bookName];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-
+var bookName = 'Introdução HTML';
+console.log('O livro ' + bookName + ' tem ' + book(bookName).quantidadePaginas + ' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-
+var bookName2 = 'Skin in the game';
+console.log('O autor do livro ' + bookName2 + ' é ' + book(bookName2).autor + '.');
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-
+var bookName3 = 'CSS avançado';
+console.log('O livro ' + bookName3 + ' foi publicado pela editora ' + book(bookName3).editora + '.')
