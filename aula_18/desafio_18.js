faf275892139c25257774983b08f949911947be7
Fernando Saraiva <fpsaraiva@gmail.com>
2020-02-09 23:05:41 -0300

    desafio aula 18 - parte 3

--- aula_18/desafio_18.js
+++ aula_18/desafio_18.js
@@ -97,5 +97,9 @@
   corretas, para depois aplicar no código ;)
   */
   console.log( '\nFazer replace dos textos das tags:' );
-  // ?
+  console.log(
+    '<h1>Título da página</h1><p>Este é um parágrafo</p><footer>Rodapé</footer>'
+    .replace(/<(\w+)>([^<]+)<\/\w+>/g, 
+    '<$1>O texto dentro da tag "$1" é "$2"</$1>\n'));
+
 })();
