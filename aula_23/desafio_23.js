e706585649c31b7642caadb19eecf92196af7aa4
Fernando Saraiva <fpsaraiva@gmail.com>
2020-02-17 23:16:11 -0300

    desafio aula 23 - parte 4

--- aula_23/desafio_23.js
+++ aula_23/desafio_23.js
@@ -29,6 +29,7 @@
   var $buttonsNumbers = doc.querySelectorAll('[data-js="button-number"]');
   var $buttonsOperations = doc.querySelectorAll('[data-js="button-operation"]');
   var $buttonCE = doc.querySelector('[data-js="button-ce"]');
+  var $buttonEqual = doc.querySelector('[data-js="button-equal"]');
 
   Array.prototype.forEach.call($buttonsNumbers, function(button) {
     button.addEventListener('click', handleClickNumber, false);
@@ -39,22 +40,38 @@
   });
 
   $buttonCE.addEventListener('click', handleClickCE, false);
+  $buttonEqual.addEventListener('click', handleClickEqual, false);
 
   function handleClickNumber() {
     $visor.value += this.value;
   }
 
   function handleClickOperation() {
-    var operations = ['+', '-', 'x', '/'];
-    if(isLastItemAnOperation(operations)) {
-      $visor.value = $visor.value.slice(0, -1);
-    }
+    removeLastItemIfItIsAnOperator();
 
     $visor.value += this.value;
   }
 
   function handleClickCE() {
     $visor.value = 0;
+  }
+
+  function isLastItemAnOperation() {
+    var operations = ['+', '-', 'x', '/'];
+    var lastItem = $visor.value.split('').pop();
+    return operations.some(function(operator) {
+      return operator === lastItem;
+    });
+  }
+
+  function removeLastItemIfItIsAnOperator() {
+    if(isLastItemAnOperation()) {
+      $visor.value = $visor.value.slice(0, -1);
+    }
+  }
+
+  function handleClickEqual() {
+    removeLastItemIfItIsAnOperator();
   }
 
 })(window, document);
