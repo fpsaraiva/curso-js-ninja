1b55a6376791d2bcac1552291664883191d6b005
Fernando Saraiva <fpsaraiva@gmail.com>
2020-02-18 00:20:05 -0300

    desafio aula 23 - parte 6

--- aula_23/desafio_23.js
+++ aula_23/desafio_23.js
@@ -47,8 +47,7 @@
   }
 
   function handleClickOperation() {
-    removeLastItemIfItIsAnOperator();
-
+    $visor.value = removeLastItemIfItIsAnOperator($visor.value);
     $visor.value += this.value;
   }
 
@@ -56,25 +55,39 @@
     $visor.value = 0;
   }
 
-  function isLastItemAnOperation() {
+  function isLastItemAnOperation(number) {
     var operations = ['+', '-', 'x', '/'];
-    var lastItem = $visor.value.split('').pop();
+    var lastItem = number.split('').pop();
     return operations.some(function(operator) {
       return operator === lastItem;
     });
   }
 
-  function removeLastItemIfItIsAnOperator() {
-    if(isLastItemAnOperation()) {
-      $visor.value = $visor.value.slice(0, -1);
+  function removeLastItemIfItIsAnOperator(number) {
+    if(isLastItemAnOperation($visor.value)) {
+      return number.slice(0, -1);
     }
+    return number;
   }
 
   function handleClickEqual() {
-    removeLastItemIfItIsAnOperator();
+    $visor.value = removeLastItemIfItIsAnOperator($visor.value);
     var allValues = $visor.value.match(/\d+[+x/-]?/g);
-    var result = allValues.reduce(function(accumulated, actual) {
-      return accumulated + actual;
+    $visor.value = allValues.reduce(function(accumulated, actual) {
+      var firstValue = accumulated.slice(0, -1);
+      var operator = accumulated.split('').pop();
+      var lastValue = removeLastItemIfItIsAnOperator(actual);
+      var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
+      switch(operator) {
+        case '+':
+          return (Number(firstValue) + Number(lastValue)) + lastOperator;
+        case '-':
+          return (Number(firstValue) - Number(lastValue)) + lastOperator;
+        case 'x':
+          return (Number(firstValue) * Number(lastValue)) + lastOperator;
+        case '/':
+          return (Number(firstValue) / Number(lastValue)) + lastOperator;
+      }
     });
   }
 
