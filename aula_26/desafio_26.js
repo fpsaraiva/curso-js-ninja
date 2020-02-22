9b51bb3da8ac4c742a5d906e164af0b3b8e2be61
Fernando Saraiva <fpsaraiva@gmail.com>
2020-02-22 11:26:35 -0300

    desafio aula 26 - parte 2

--- aula_26/desafio_26.js
+++ aula_26/desafio_26.js
@@ -23,21 +23,30 @@
   // ?
 
   function DOM(elements) {
-    this.element = this.getDOMElements(elements);
+    this.element = document.querySelectorAll(elements);
   }
 
-  DOM.prototype.getDOMElements = function getDOMElements() {
-    return document.querySelectorAll(elements);
+  DOM.prototype.on = function on(eventType, callback) {
+    Array.prototype.forEach.call(this.element, function(element) {
+      element.addEventListener(eventType, callback, false);
+    });
   };
 
-  DOM.prototype.on = function on() {};
-  DOM.prototype.on = function off() {};
-  DOM.prototype.on = function get() {};
+  DOM.prototype.off = function off(eventType, callback) {
+    Array.prototype.forEach.call(this.element, function(element) {
+      element.removeEventListener(eventType, callback, false);
+    });
+  };
+
+  DOM.prototype.get = function get() {
+    return this.element;
+  };
 
   var $a = new DOM('[data-js="link"]');
-  $a.on('click', function(e) {
+  $a.on('click', function handleClick(e) {
     e.preventDefault();
     console.log('clicou');
+    $a.off('click', handleClick)
   });
 
   console.log('Elementos selecionados:', $a.get());
