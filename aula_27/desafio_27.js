1c109fc2052960a25215fca53bd5f5f0109751b5
Fernando Saraiva <fpsaraiva@gmail.com>
2020-02-25 15:09:53 -0300

    desafio aula 27 - parte 2

--- aula_27/desafio_27.js
+++ aula_27/desafio_27.js
@@ -43,18 +43,60 @@
     return Array.prototype.forEach.apply(this.element, arguments);
   };
 
-  //DOM.prototype.
-  //DOM.prototype.
-  //DOM.prototype.
-  //DOM.prototype.
-  //DOM.prototype.
-  //DOM.prototype.
+  DOM.prototype.map = function map() {
+    return Array.prototype.map.apply(this.element, arguments);
+  };
 
-  var $a = new DOM('[data-js="link"]');
+  DOM.prototype.filter = function filter() {
+    return Array.prototype.filter.apply(this.element, arguments);
+  };
 
-  console.log($a);
-  $a.forEach(function(item, index) {
-    console.log(item.firstChild.nodeValue);
-  });
+  DOM.prototype.reduce = function reduce() {
+    return Array.prototype.reduce.apply(this.element, arguments);
+  };
+
+  DOM.prototype.reduceRight = function reduceRight() {
+    return Array.prototype.reduceRight.apply(this.element, arguments);
+  };
+
+  DOM.prototype.every = function every() {
+    return Array.prototype.every.apply(this.element, arguments);
+  };
+
+  DOM.prototype.some = function some() {
+    return Array.prototype.some.apply(this.element, arguments);
+  };
+
+  DOM.prototype.isArray = function isArray(param) {
+    return Object.prototype.toString.call(param) === '[object Array]';
+  };
+
+  DOM.prototype.isObject = function isObject(param) {
+    return Object.prototype.toString.call(param) === '[object Object]';
+  };
+
+  DOM.prototype.isFunction = function isFunction(param) {
+    return Object.prototype.toString.call(param) === '[object Function]';
+  };
+
+  DOM.prototype.isNumber = function isNumber(param) {
+    return Object.prototype.toString.call(param) === '[object Number]';
+  };
+
+  DOM.prototype.isString = function isString(param) {
+    return Object.prototype.toString.call(param) === '[object String]';
+  };
+
+  DOM.prototype.isBoolean = function isBoolean(param) {
+    return Object.prototype.toString.call(param) === '[object Boolean]';
+  };
+
+  DOM.prototype.isNull = function isNull(param) {
+    return Object.prototype.toString.call(param) === '[object Null]'
+    || Object.prototype.toString.call(param) === '[object Undefined]';
+  };
+
+  var DOM = new DOM();
+  console.log(DOM.isBoolean(false));
 
 })();
