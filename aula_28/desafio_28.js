449e37d8c6e5d6e73c5f8a7860c8d534521a87ff
Fernando Saraiva <fpsaraiva@gmail.com>
2020-03-03 09:01:29 -0300

    desafio aula 28 - parte 4

--- aula_28/desafio_28.js
+++ aula_28/desafio_28.js
@@ -99,6 +99,12 @@
 
   var $formCEP = new DOM('[data-js="form-cep"]');
   var $inputCEP = new DOM('[data-js="input-cep"]');
+  var $logradouro = new DOM('[data-js="logradouro"]');
+  var $bairro = new DOM('[data-js="bairro"]');
+  var $estado = new DOM('[data-js="estado"]');
+  var $cidade = new DOM('[data-js="cidade"]');
+  var $cep = new DOM('[data-js="cep"]');
+  var $status = new DOM('[data-js="status"]');
   var ajax = new XMLHttpRequest();
   $formCEP.on('submit', handleSubmitFormCEP);
 
@@ -107,17 +113,21 @@
     var url = getUrl();
     ajax.open('GET', url);
     ajax.send();
+    getMessage('loading');
     ajax.addEventListener('readystatechange', handleReadyStateChange);
   }
 
   function getUrl() {
-    return 'https://apps.widenet.com.br/busca-cep/api/cep/[CEP].json'.replace(
-      '[CEP]', 
-      $inputCEP.get()[0].value.replace(/\D/g, ''));
+    return replaceCEP('https://apps.widenet.com.br/busca-cep/api/cep/[CEP].json');
+  }
+
+  function clearCEP() {
+    return $inputCEP.get()[0].value.replace(/\D/g, '');
   }
 
   function handleReadyStateChange() {
     if(isRequestOk()) {
+      getMessage('ok');
       fillCEPFields();
     }
   }
@@ -128,19 +138,26 @@
 
   function fillCEPFields() {
     var data = parseData();
-    if(!data)
-      return console.log('DATA ERROR', data);
-    console.log('DATA', data);
-    var $logradouro = new DOM('[data-js="logradouro"]');
-    var $bairro = new DOM('[data-js="bairro"]');
-    var $estado = new DOM('[data-js="estado"]');
-    var $cidade = new DOM('[data-js="cidade"]');
-    var $cep = new DOM('[data-js="cep"]');
+    if(!data) {
+      getMessage('error');
+      data = clearData();
+    }
+
     $logradouro.get()[0].textContent = data.address;
     $bairro.get()[0].textContent = data.district;
     $estado.get()[0].textContent = data.state;
     $cidade.get()[0].textContent = data.city;
     $cep.get()[0].textContent = data.code;
+  }
+
+  function clearData() {
+    return {
+      address: '-',
+      district: '-',
+      state: '-',
+      city: '-', 
+      code: '-'
+    }
   }
 
   function parseData() {
@@ -154,13 +171,17 @@
     return result;
   }
 
-  function getMessage() {
+  function getMessage(type) {
     var messages = {
-      loading: 'Buscando informações para o CEP [CEP]...',
-      ok: 'Endereço referente ao CEP [CEP]:',
-      error: 'Não encontramos o endereço para o CEP [CEP].'
-    }
-    return messages[type];
+      loading: replaceCEP('Buscando informações para o CEP [CEP]...'),
+      ok: replaceCEP('Endereço referente ao CEP [CEP]:'),
+      error: replaceCEP('Não encontramos o endereço para o CEP [CEP].')
+    };
+    $status.get()[0].textContent = messages[type];
+  }
+
+  function replaceCEP(message) {
+    return message.replace('[CEP]', clearCEP());
   }
 
 })();
